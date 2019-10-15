998c22907e5d03724aba16dce3a92b59e11fa2aa
Shaquon <shaquonkelley@gmail.com>
2019-10-15 11:18:51 -0400

    fixed linter errors

--- numseq/geo.py
+++ numseq/geo.py
@@ -2,8 +2,10 @@
 3 functions, square, triangle, cube. 
 """
 
+
 def square(n):
     return n**2
+
 
 def triangle(n):
     result = 0
@@ -11,6 +13,7 @@ def triangle(n):
         result = result + n
         n = n - 1
     return result
+
 
 def cube(n):
     return n**3
